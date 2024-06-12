8c86346d94a574efb7300b0a7b633b39f5ee1bc3
Krasimir Dramaliev <krasipeace@abv.bg>
2024-06-12 21:20:38 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import GifHandler from "@/components/GifHandler";
 export default function Home() {
 	return (
 		<div>
-			<div className="flex justify-center">
-				<h1 className="homePageTitle bg-gradient-to-r from-purple-600 via-pink-500 to-purple-600 inline-block text-transparent bg-clip-text">Welcome to HeartBound</h1>
-			</div>
 			<GifHandler gifUrl="https://i.imgur.com/v2pbvcz.gif" altText="front-page-image" width="1152" height="648" />
 		</div>
 	);
