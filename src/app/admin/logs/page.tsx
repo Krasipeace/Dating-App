280c1504785b0ac4ee4bf461aed8c5331b70ca78
Krasimir Dramaliev <krasipeace@abv.bg>
2025-01-10 15:59:56 +0200

    Update page.tsx

--- src/app/admin/logs/page.tsx
+++ src/app/admin/logs/page.tsx
@@ -1,6 +1,8 @@
 import { getAuditLogs } from "@/app/actions/adminActions";
 import Logs from "./Logs";
 
+export const dynamic = "force-dynamic";
+
 export default async function LogsPage() {
     const logs = await getAuditLogs();
 
